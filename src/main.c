3de2569686cb2711c30796bc9febac2d76b825ef
Martin Marcucci <rmarku@gmail.com>
2016-08-11 11:47:31 -0300

    se implemento delay

--- src/main.c
+++ src/main.c
@@ -14,27 +14,39 @@
 
 void APP_1ms(void);
 
+void delay(uint16_t delay_ms);
+
+volatile uint16_t counter = 0;
+
 int main(void) {
 
 	BSP_Init();
 
 	while (1) {
 
-
-
+		LedOn(LED_AZUL);
+		delay(1000);
+		LedOff(LED_AZUL);
+		LedOn(LED_ROJO);
+		delay(1000);
+		LedOff(LED_ROJO);
+		LedOn(LED_NARANJA);
+		delay(1000);
+		LedOff(LED_NARANJA);
+		LedOn(LED_VERDE);
+		delay(1000);
+		LedOff(LED_VERDE);
 	}
 }
 
-void APP_1ms(void){
-	static uint16_t counter = ONE_SECOND;
-
-		if (counter) {
-			counter--;
-			if (!counter) {
-				LedToggle(ALL_LEDS);
-				counter = ONE_SECOND;
-			}
-		}
+void delay(uint16_t delay_ms) {
+	counter = delay_ms;
+	while(counter);
 }
 
+void APP_1ms(void) {
+	if (counter) {
+		counter--;
+	}
+}
 
