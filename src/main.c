1de8c428ed6443841e75a37fbdc9f224e45ffae1
Matias Labedz <matias.labedz@gmail.com>
2016-08-11 00:16:25 -0300

    se agrego la configuración del clk

--- src/main.c
+++ src/main.c
@@ -25,6 +25,34 @@ void APP_ISR_1ms(void);
 TIM_HandleTypeDef TIM2_Handle;
 
 int main(void) {
+
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	__PWR_CLK_ENABLE()
+	;
+
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1
+			| RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+
+
 	__GPIOD_CLK_ENABLE()
 	;
 
@@ -40,8 +68,8 @@ int main(void) {
 	;
 
 	TIM2_Handle.Instance = TIM2;
-	TIM2_Handle.Init.Period = 1000;
-	TIM2_Handle.Init.Prescaler = 168;
+	TIM2_Handle.Init.Period = 1000 - 1;
+	TIM2_Handle.Init.Prescaler = 86 - 1;
 	TIM2_Handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	TIM2_Handle.Init.CounterMode = TIM_COUNTERMODE_UP;
 
